3e89bf30f318a371da3701e80fb1ffcd2d234214
unknown <amirreza13.7676@gmail.com>
2026-04-26 23:20:25 +0330

    player component is being added

---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditting, setIsEditting] = useState(false);
+  function handleEditClick() {
+    setIsEditting((editing) => !editing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  //   let btnCaption = "Edit";
+
+  if (isEditting) {
+    playerName = (
+      <input type="text" required placeholder="enter your name" value={name} />
+    );
+    // btnCaption = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditting ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
